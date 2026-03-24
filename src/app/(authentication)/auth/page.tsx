5c87d79c1e9f639f7d12b31bfa6c2dd62a853b61
huy1130 <huy338119@gmail.com>
2026-03-25 00:16:59 +0700

    fix UI login, register and do not delete product - only change status

--- src/app/(authentication)/auth/page.tsx
+++ src/app/(authentication)/auth/page.tsx
@@ -406,11 +406,11 @@ export default function AuthPage() {
                       </button>
                     </form>
 
-                    <div className="mt-8 text-center animate-[fadeIn_0.8s_ease-out_0.5s_both]">
+                    {/* <div className="mt-8 text-center animate-[fadeIn_0.8s_ease-out_0.5s_both]">
                       <p className="text-sm text-gray-600">
                         or login with social platforms
                       </p>
-                    </div>
+                    </div> */}
                   </div>
                 ) : (
                   /* Register Form */
@@ -597,7 +597,7 @@ export default function AuthPage() {
 
                 {/* Social Login Icons - Common for both */}
                 <div className="mt-6 flex justify-center gap-4 animate-[fadeIn_1s_ease-out_0.6s_both]">
-                  <button
+                  {/* <button
                     className="w-12 h-12 rounded-full border-2 border-gray-200 flex items-center justify-center hover:border-blue-500 hover:bg-blue-50 transition-all hover:scale-110"
                     title="Google"
                   >
@@ -619,7 +619,7 @@ export default function AuthPage() {
                         d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                       />
                     </svg>
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
